ecad4352b8c49caaed560d551db4e7c4062c8bf3
rasel mridha <raselmridha792@gmail.com>
2024-09-19 20:33:51 +0600

    Verify Pin Number and add money to the balance

--- home.js
+++ home.js
@@ -5,9 +5,26 @@ document.getElementById('btn-add-money')
 
         // step-2 get money from input 
         const getMoney = document.getElementById('add-money-input').value;
-        console.log(getMoney);
 
         // step-3 get the pin number validation 
         const checkPin = document.getElementById('pin-number').value;
-        console.log(checkPin);
+        if(checkPin === '9099'){
+
+            // step-4 get the current balance 
+            const currentBalance = document.getElementById('account-balance').innerText;
+
+            // convert the string to number input and available balances
+            const currentBalanceNumber = parseFloat(currentBalance);
+            const getMoneyNumber = parseFloat(getMoney);
+
+            // step-5: update the balance amount 
+            const updatedBalance = currentBalanceNumber + getMoneyNumber;
+            console.log(updatedBalance);
+
+            // step-6: update the balance to the ui 
+            document.getElementById('account-balance').innerText = updatedBalance;
+        }
+        else{
+            alert('sorry! wrong pin number')
+        }
 })
